16d8189d8a5541eb13d8cf2f7d908ca0eb78ad97
itigoame <rikuan1128@gmail.com>
2022-09-30 09:27:05 +0000

    fix user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum gender: { women: 0, men: 1 }
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :user, controllers:{
+    registrations: "user/registrations",
+    sessions:      "user/sessions"
+  }
+
+  devise_for :admin, controllers:{
+    registrations: "admin/registrations",
+    sessions:      "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
