14dd4287c812eeb689030323dea1753ba5367654
itigoame <rikuan1128@gmail.com>
2022-10-01 03:19:56 +0000

    gender

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,2 +1,25 @@
 class User::UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+
+  end
+
+  def index
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :gender)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'home' => "homes#top"
+
+  root to: "homes#top"
+
+
   devise_for :user, controllers:{
     registrations: "user/registrations",
     sessions:      "user/sessions"
@@ -8,5 +13,7 @@ Rails.application.routes.draw do
     registrations: "admin/registrations",
     sessions:      "admin/sessions"
   }
+  
+  resources :users, only: [:index, :show, :edit, :update] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
